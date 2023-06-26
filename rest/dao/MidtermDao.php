4423d305923d90291cec5bd024d0930a61444bd4
filipljoljic <filip.ljoljic@stu.ibu.edu.ba>
2023-06-26 15:57:11 +0200

    connection

---
+++ rest/dao/MidtermDao.php
@@ -0,0 +1,32 @@
+<?php
+require_once "BaseDao.php";
+
+class MidtermDao extends BaseDao {
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    /** TODO
+    * Implement DAO method used add new investor to investor table and cap-table
+    */
+    public function investor(){
+
+    }
+
+    /** TODO
+    * Implement DAO method to validate email format and check if email exists
+    */
+    public function investor_email($email){
+
+    }
+
+    /** TODO
+    * Implement DAO method to return list of investors according to instruction in MidtermRoutes.php
+    */
+    public function investors(){
+
+    }
+
+}
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.8.0',
+            'version' => '6.8.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'reference' => '48b0210c51718d682e53210c24d25c5a10a2299b',
+            'dev_requirement' => false,
+        ),
+        'mikecao/flight' => array(
+            'pretty_version' => 'v2.0.1',
+            'version' => '2.0.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mikecao/flight',
+            'aliases' => array(),
+            'reference' => 'a130231646e6c7a9e2504a9025f851e9a3bf1975',
+            'dev_requirement' => false,
+        ),
+    ),
+);
